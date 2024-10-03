e56263680278293c475d1d374d209a580d992120
Jaheen27 <146910613+Jaheen27@users.noreply.github.com>
2024-10-03 09:27:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.getElementById("exploreBtn").addEventListener("click", function (e) {
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-    countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    countdownElement.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
   
     if (distance < 0) {
       clearInterval(x);
